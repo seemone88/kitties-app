e2264c90eb6445100cc9b6b371cd5d422c7a6896
Simon Cho <simon.cho2093@gmail.com>
2015-08-25 09:12:38 -0700

    kitties app

---
+++ kitties-STARTER-app/js/KittiesController.js
@@ -0,0 +1,42 @@
+(function(){
+	angular
+			.module("kittiesApp")
+			.controller("KittiesController", KittiesController);
+			function KittiesController(){
+				var self = this;
+
+
+				self.kittiesList=
+				[
+						{
+							catName: 'Karl',
+							monthsOld: 5,
+							gender: 'boy',
+							image: "images/carl.png",
+							adopted: true,
+						},
+						{
+							catName: 'Jack',
+							monthsOld: 4,
+							gender: 'boy',
+							image: "images/jack.png",
+							adopted: false
+						},
+						{
+							catName: 'Oscar',
+							monthsOld: 2,
+							gender: 'boy',
+							image: "images/oscar.png",
+							adopted: false
+						},
+						{
+							catName: 'Princess Mew',
+							monthsOld: 3,
+							gender: 'girl',
+							image: "images/princessmew.png",
+							adopted: false
+						}
+
+				]
+			};
+})();
